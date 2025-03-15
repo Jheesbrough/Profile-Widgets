c13c3649a81aff97b6be5a1daf3024dfe49ad652
Jonathan <71599416+Jheesbrough@users.noreply.github.com>
2025-03-15 18:48:22 +0000

    Fixed run

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-import { captureScreenshot } from "./screenshot.js";
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { captureScreenshot } from './screenshot.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const main = async () => {
-  const url = "https://jheesbrough.dev";
-  const outputPath = path.join(__dirname, "screenshot.svg");
-  const options = {
-    viewport: { width: 1920, height: 1080 },
-    wait: 1000,
-  };
+const settingsPath = path.join(__dirname, 'settings.json');
+const settings = JSON.parse(fs.readFileSync(settingsPath, 'utf-8'));
 
-  await captureScreenshot(url, outputPath, options);
+const main = async () => {
+  const folderName = path.join('Widgets', 'screenshots');
+
+  if (!fs.existsSync(path.join(__dirname, folderName))) {
+    fs.mkdirSync(path.join(__dirname, folderName), { recursive: true });
+  }
+
+  if (settings.widgets.screenshots) {
+    for (const url of settings.widgets.screenshots.urls) {
+      const fileName = `${url
+        .replace(/https?:\/\//, '')
+        .replace(/\//g, '_')}.svg`;
+      const outputPath = path.join(__dirname, folderName, fileName);
+
+      const options = {
+        viewport: { width: 1920, height: 1080 },
+        wait: 1000,
+      };
+
+      await captureScreenshot(url, outputPath, options);
+    }
+  }
 };
 
 main().catch((error) => {
-  console.error("Error:", error);
+  console.error('Error:', error);
 });
